f6f37a99ba473d8052b70c7281a31be6ae500481
Milap Dave <dave.milap3@gmail.com>
2023-08-28 21:23:49 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -64,7 +64,7 @@ export default function Header() {
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="2"
+                    strokeWidth="2"
                   >
                     <path
                       strokeLinecap="round"
